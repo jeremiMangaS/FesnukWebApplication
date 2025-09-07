1ef4c17d48b8823e5d9a51ac62a1be573225db34
Mangatur Jeremi Silitonga <>
2025-09-07 14:36:00 +0700

    Initial commit backend project

---
+++ src/main/java/com/mycompany/fesnukwebapp/FesnukWebApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fesnukwebapp;
+
+/**
+ *
+ * @author Amanda Sentani
+ */
+public class FesnukWebApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
